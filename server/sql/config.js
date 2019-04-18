1641d51f3d1f08f444cf2b9cb1a889bee8153c12
Tommy <tommylin967@gmail.com>
2019-04-17 18:34:52 -0700

    make it better

--- server/sql/config.js
+++ server/sql/config.js
@@ -1,8 +1,8 @@
 let config = {
-    host:'localhost',
-    user:'root',
-    password:'password',
-    database:'spartanhotel'
+    host:'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user:'alkvhp2kk494o40f',
+    password:'q1cdya6eibxrb6zx',
+    database:'qrfou6fx5u47heqq'
 }
 
 module.exports = config
