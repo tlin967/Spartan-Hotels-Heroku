73763abb3071d80a6d3283564c3646539141224d
Tommy <tommylin967@gmail.com>
2019-04-18 15:45:47 -0700

    Make it better

--- server/sql/config.js
+++ server/sql/config.js
@@ -1,8 +1,17 @@
+/*
 let config = {
     host:'localhost',
     user:'root',
     password:'password',
     database:'spartanhotel'
 }
+*/
 
+//For heroku
+let config = {
+    host:'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user:'qqunus9bd6bvdgrx',
+    password:'tg0v7tz16ilqigay',
+    database:'rs3a661h7sck1biq'
+}
 module.exports = config
