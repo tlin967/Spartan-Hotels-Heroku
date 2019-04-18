dcf0901b861555ccdd9789c69744abc2f7309877
Tommy <tommylin967@gmail.com>
2019-04-18 15:50:01 -0700

    Make it better

---
+++ client/build/precache-manifest.6724e3d9a97fc24fed76ce9cd919faba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "066bdae802f379a979d374ae6ee726b3",
+    "url": "/static/media/homeImage7.066bdae8.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c725caa589b31b2f3267",
+    "url": "/static/js/main.c725caa5.chunk.js"
+  },
+  {
+    "revision": "676b06f8d31cd7ee83c4",
+    "url": "/static/js/2.676b06f8.chunk.js"
+  },
+  {
+    "revision": "c725caa589b31b2f3267",
+    "url": "/static/css/main.2d2ae6cf.chunk.css"
+  },
+  {
+    "revision": "676b06f8d31cd7ee83c4",
+    "url": "/static/css/2.caa940ea.chunk.css"
+  },
+  {
+    "revision": "17186a3fec1e0d441de3232f68d4f743",
+    "url": "/index.html"
+  }
+];
