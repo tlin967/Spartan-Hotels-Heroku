7310cb249c903e9f3ddb961acde205149faa21d7
Tommy <tommylin967@gmail.com>
2019-04-19 15:22:44 -0700

    Changed Db

--- server/sql/config.js
+++ server/sql/config.js
@@ -9,7 +9,7 @@ let config = {
 
 //For heroku
 let config = {
-    host:'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+    host:'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user:'iiwv47vkofr0d5c9',
     password:'qarepmx8y26jp7ic',
     database:'qzsp4l888kg17t5l'
