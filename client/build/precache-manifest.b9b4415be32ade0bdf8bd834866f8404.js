1e105e608f62209d69c89cbde2c28ca0af616782
Tommy <tommylin967@gmail.com>
2019-05-06 04:06:40 -0700

    Hope

---
+++ client/build/precache-manifest.b9b4415be32ade0bdf8bd834866f8404.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5eeb453c554341be2e72a268d83e29c",
+    "url": "/static/media/logo.e5eeb453.png"
+  },
+  {
+    "revision": "066bdae802f379a979d374ae6ee726b3",
+    "url": "/static/media/homeImage7.066bdae8.jpg"
+  },
+  {
+    "revision": "fe9904913d1cc1622e78a130a99f36db",
+    "url": "/static/media/homeImage14.fe990491.jpeg"
+  },
+  {
+    "revision": "3ededb3f90283a7c8c5543cd225002cf",
+    "url": "/static/media/homeImage.3ededb3f.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d63bda80a9735791260",
+    "url": "/static/js/main.f946a9c8.chunk.js"
+  },
+  {
+    "revision": "80ee36ce33b23d7003a0",
+    "url": "/static/js/2.0eb3b967.chunk.js"
+  },
+  {
+    "revision": "4d63bda80a9735791260",
+    "url": "/static/css/main.156fa617.chunk.css"
+  },
+  {
+    "revision": "80ee36ce33b23d7003a0",
+    "url": "/static/css/2.caa940ea.chunk.css"
+  },
+  {
+    "revision": "3df0c80542a182cd9318bbd493bc7785",
+    "url": "/index.html"
+  }
+];
