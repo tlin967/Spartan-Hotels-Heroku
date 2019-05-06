d3903d73cd76fcd38e2420c292b5a56661014865
Tommy <tommylin967@gmail.com>
2019-05-06 03:12:18 -0700

    Something

--- server/app.js
+++ server/app.js
@@ -18,6 +18,19 @@ const sessionStore = new MySQLStore(config)
 
 app.use(express.static("../client/build"));
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://spartan-hotels.herokuapp.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
 app.use(session({
   secret: 'keybfsdoardafsa cat',
   store: sessionStore,
